900d79dc67af3c8848f170c306a71bc41b5ed2bb
thanwarat <maijang270@gmail.com>
2026-03-30 14:46:42 +0700

    lab13

--- src/app/google/config.ts
+++ src/app/google/config.ts
@@ -3,7 +3,7 @@ import { OauthConfiguration } from './types/services';
 
 export const googleOauthConfig: OauthConfiguration = {
   id: '209689905225-dj1bo29m0c7or5926cv4bb1nu5aru0cv.apps.googleusercontent.com',
-  secret: '',
+  secret: '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',
   tokenUrl: 'https://oauth2.googleapis.com/token',
   authorizationUrl: 'https://accounts.google.com/o/oauth2/v2/auth',
   redirectUri: isDevMode()
